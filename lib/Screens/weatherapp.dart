9798783623636d011cdba512da3e5d7dbff8cf5d
shatilsarower <shatild12@gmail.com>
2021-05-02 00:20:14 +0600

    5th

---
+++ lib/Screens/weatherapp.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class WeatherApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Weather",
+        ),
+        elevation: 2.0,
+      ),
+      body: Container(
+        child: Center(
+          child: Text("Weather",
+              style: GoogleFonts.lato(
+                fontSize: 24,
+              )),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'Screens/weatherapp.dart';
+
 void main() => runApp(
       MaterialApp(
         debugShowCheckedModeBanner: false,
@@ -9,93 +11,16 @@ void main() => runApp(
       ),
     );
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            color: Color(0xfff1f1f1),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Location",
-                  style: TextStyle(
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.w300,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(
-                    top: 10,
-                    bottom: 10,
-                  ),
-                  child: Text(
-                    "Tem",
-                    style: TextStyle(
-                      color: Colors.deepPurpleAccent,
-                      fontWeight: FontWeight.w300,
-                      fontSize: 40.0,
-                    ),
-                  ),
-                ),
-                Text(
-                  "High of temp, low of temp",
-                  style: TextStyle(
-                    color: Color(0xff9e9e9e),
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.all(20.0),
-              child: ListView(
-                children: [
-                  ListTile(
-                    leading: Column(
-                      //mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.thermostat_outlined,
-                          color: Colors.deepPurpleAccent,
-                        ),
-                      ],
-                    ),
-                    title: Text(
-                      "Tempareture",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 18.0,
-                      ),
-                    ),
-                    subtitle: Text(
-                      "Subtitle",
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w600,
-                        color: Color(0xff9e9e9e),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      home: WeatherApp(),
     );
   }
 }
