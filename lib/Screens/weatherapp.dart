efad1b2eb2c64dfdb76844ef4ed5ef6c9e57c395
shatilsarower <shatild12@gmail.com>
2021-05-02 01:02:02 +0600

    8th

--- lib/Screens/weatherapp.dart
+++ lib/Screens/weatherapp.dart
@@ -1,25 +1,47 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class WeatherApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(
+            Icons.search,
+            size: 30,
+            color: Colors.white,
+          ),
+        ),
+        actions: [
+          Container(
+            margin: EdgeInsets.fromLTRB(0, 0, 20, 0),
+            child: SvgPicture.asset(
+              'asstes/menu.svg',
+              height: 30.0,
+              width: 30.0,
+              color: Colors.white,
+            ),
+          ),
+        ],
         title: Text(
           "Weather",
         ),
-        elevation: 2.0,
       ),
       body: Container(
-        child: Center(
-          child: Text(
-            "Weather",
-            style: GoogleFonts.montserrat(
-              fontSize: 24,
+        child: Stack(
+          children: [
+            Image.asset(
+              'asstes/night.jpg',
+              fit: BoxFit.cover,
+              height: double.infinity,
+              width: double.infinity,
             ),
-          ),
+          ],
         ),
       ),
     );
